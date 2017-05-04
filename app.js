b35ac5deb400c03a901834a84bb4f2ec7096eefc
Wes Reid <bwreid@gmail.com>
2017-05-04 13:24:56 -0700

    Install method-override

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-
+var methodOverride = require('method-override')
 var index = require('./routes/index');
 var parks = require('./routes/parks');
 
@@ -22,6 +22,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(methodOverride('_method'))
 
 app.use('/', index);
 app.use('/parks', parks);
