108d6442101f1d4032958afb870029ee447ef6e0
Kay Krewson <kay.k@comcast.net>
2017-05-12 13:07:56 -0700

    had to add .env stuff to knexfile for production

--- knexfile.js
+++ knexfile.js
@@ -21,4 +21,14 @@ module.exports = {
       directory: path.join(__dirname, 'db', 'seeds')
     }
   }
+  production: {
+    client: 'pg',
+    connection: process.env.DATABASE_URL,
+    migrations: {
+      directory: path.join(__dirname, 'db', 'migrations')
+    },
+    seeds: {
+      directory: path.join(__dirname, 'db', 'seeds')
+    }
+  }
 }
